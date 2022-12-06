75ca38920fd60229a85d986233a6d8cd39aac13e
Andrei <andrei.miridonov163@gmail.com>
2022-12-06 20:13:45 +0500

    finaltask

---
+++ FinalTask/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] array1 = new string[5] {"888", "34523", "Gek", "brains", "001"};
+string[] array2 = new string[array1.Length];
+
+ArrayReforming(array1, array2);
+PrintArray(array2);
+
+void ArrayReforming(string[] array1, string[] array2)
+{
+    
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[i] = array1[i];
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
